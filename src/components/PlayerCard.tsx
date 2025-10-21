203fe2dc08281ce9e9722cf0b697749009378618
Marcus Silva <marcus.silva@forpeople.io>
2025-10-21 16:47:35 -0300

    feat: cria componente animado de Card

---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,50 @@
+import { Trophy } from "lucide-react";
+
+interface PlayerCardProps {
+  name: string;
+  total_wins: number;
+  actual_wins: number;
+  trophies: number;
+  path: string;
+}
+
+const PlayerCard = ({
+  name,
+  total_wins,
+  actual_wins,
+  trophies,
+  path,
+}: PlayerCardProps) => {
+  return (
+    <div className="group h-60 w-40 [perspective:1000px]">
+      <div className="relative h-full w-full transition-transform duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+        {/* Frente */}
+        <div className="bg-card-front absolute inset-0 flex items-center justify-center rounded-xl shadow-lg backface-hidden">
+          <img
+            src={path}
+            alt={name}
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+
+        {/* Verso */}
+        <div className="bg-card-back absolute inset-0 flex [transform:rotateY(180deg)] flex-col items-center justify-center gap-2 rounded-xl text-center shadow-lg backface-hidden">
+          <h1 className="text-foreground w-32 truncate text-lg font-bold">
+            {name}
+          </h1>
+          <div className="flex items-center gap-1">
+            <Trophy className="size-8 text-yellow-500" />
+            <h2>{trophies}</h2>
+          </div>
+          <span className="text-muted-foreground text-xs">
+            Vitórias Totais: {total_wins}
+            <br />
+            Vitórias Atuais: {actual_wins}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerCard;
